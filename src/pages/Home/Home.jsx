1e29529838319ab63373eed9d20d3f545d0ebac1
1amaks <ayushsahu.181.kpt@gmail.com>
2022-12-01 13:43:26 +0530

    minor changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,17 +18,17 @@ const Home = () => {
 			<div className="main-home">
 				<div className="aboutUs">
 					<Tada>
-					<div className="homeLogoMain">
-						<img src={Alumnlogo} />
-						
-						 <p className="Alumninest">Alumni Nest</p>
-						 </div>
+						<div className="homeLogoMain">
+							<img src={Alumnlogo} />
+
+							<p className="Alumninest">Alumni Nest</p>
+						</div>
 						{/*<p className="quotem">-Connecting Network</p>
 					</div>
 					<div>
 						<p className="quote1">"I think the success of any school can be measured by the contribution the alumni make to our national life." <br></br> ~ John F. Kennedy</p>
 					</div> */}
-					<div id="container">
+						<div id="container">
 							Creating !
 							<div id="flip">
 								<div><div>Building Networks</div></div>
@@ -37,7 +37,7 @@ const Home = () => {
 							</div>
 							AweSoMe!
 						</div>
-						
+
 					</Tada>
 					<Fade left>
 						<div className="aboutUsBottom">
@@ -67,24 +67,24 @@ const Home = () => {
 						<div className="recentEventsCardGroup">
 							<div className="recentEventsCard">
 								<div className="eventImage">
-									<img src="https://images.pexels.com/photos/2747449/pexels-photo-2747449.jpeg?cs=srgb&dl=pexels-wolfgang-2747449.jpg&fm=jpg" alt="Event1"/>
+									<img src="https://images.pexels.com/photos/2747449/pexels-photo-2747449.jpeg?cs=srgb&dl=pexels-wolfgang-2747449.jpg&fm=jpg" alt="Event1" />
 								</div>
 								<p className="eventName">Alumni Fest</p>
 								<p className="eventDesc">19 . NOV . 2022</p>
-								
-							</div>
-							<div className="recentEventsCard">
-								<div className="eventImage">
-									<img src="https://almashines.s3.dualstack.ap-southeast-1.amazonaws.com/assets/media/images/169_1640419827_96abfa9e65d5d604c010fb63ea95cdd5.jpeg" alt="Event2"/>
-								</div>
-								<p className="eventName">Event -2</p>
-								<p className="eventDesc">04 . DEC . 2022</p>
-								
 
 							</div>
 							<div className="recentEventsCard">
 								<div className="eventImage">
-									<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKUojkfhMf4HkGKs3KJTM96LH86OeEX2qw9w&usqp=CAU" alt="Event3"/>
+									<img src="https://almashines.s3.dualstack.ap-southeast-1.amazonaws.com/assets/media/images/169_1640419827_96abfa9e65d5d604c010fb63ea95cdd5.jpeg" alt="Event2" />
+								</div>
+								<p className="eventName">Event -2</p>
+								<p className="eventDesc">04 . DEC . 2022</p>
+
+
+							</div>
+							<div className="recentEventsCard">
+								<div className="eventImage">
+									<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKUojkfhMf4HkGKs3KJTM96LH86OeEX2qw9w&usqp=CAU" alt="Event3" />
 								</div>
 								<p className="eventName">Event -3</p>
 								<p className="eventDesc">02 . JAN . 2023</p>
@@ -98,34 +98,33 @@ const Home = () => {
 			</div>
 			<Fade up>
 
-			<div className="teamInfo">
-				<img src={teamProfile} />
-			</div>
+				<div className="teamInfo">
+					<img src={teamProfile} />
+				</div>
 			</Fade>
-			<div className="whyUsBottom">
+			<Fade left>
+				<div className="whyUsBottom">
+					<div className="homeLogoContainer">
+						<img src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/Illustartion_vjqumg.png" />
+					</div>
+					<div className="aboutUsInfo">
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Deleniti labore necessitatibus velit, aspernatur suscipit voluptas
+							accusamus nisi alias animi expedita minima natus sunt quidem, ipsum
+							ipsa numquam, doloremque ducimus quibusdam? Est tempora maxime eligendi
+							placeat ducimus hic magni quis consectetur a et dicta harum quasi repellendus
+							aliquid, fugit, totam sunt nisi ratione, doloribus magnam. Assumenda amet
+							totam tempore! Nihil facilis magni eius porro nulla recusandae modi
+							repellendus, rem quos possimus sunt numquam facere, id labore? Debitis
+							reiciendis distinctio magnam, corporis blanditiis ab suscipit libero
+							maxime dolorum! Provident sunt inventore illo in quaerat reprehenderit,
+							quae quibusdam vitae tempora tempore repellendus et!</p>
+					</div>
+				</div>
+			</Fade>
 
-							<Fade left>
-							<div className="homeLogoContainer">
-								<img src="https://res.cloudinary.com/db4zymomi/image/upload/hacknitr/Illustartion_vjqumg.png"  /> 
-							</div>
-							<div className="aboutUsInfo">
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Deleniti labore necessitatibus velit, aspernatur suscipit voluptas
-									accusamus nisi alias animi expedita minima natus sunt quidem, ipsum
-									ipsa numquam, doloremque ducimus quibusdam? Est tempora maxime eligendi
-									placeat ducimus hic magni quis consectetur a et dicta harum quasi repellendus
-									aliquid, fugit, totam sunt nisi ratione, doloribus magnam. Assumenda amet
-									totam tempore! Nihil facilis magni eius porro nulla recusandae modi
-									repellendus, rem quos possimus sunt numquam facere, id labore? Debitis
-									reiciendis distinctio magnam, corporis blanditiis ab suscipit libero
-									maxime dolorum! Provident sunt inventore illo in quaerat reprehenderit,
-									quae quibusdam vitae tempora tempore repellendus et!</p>
-							</div>
-							</Fade>
-						</div>
-						
 			<Footer></Footer>
-		</div>
+		</div >
 	);
 };
 
