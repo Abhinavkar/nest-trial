ca9090ca5b09800805a0e061df63d5a9aa90bb00
1amaks <ayushsahu.181.kpt@gmail.com>
2022-12-01 20:03:31 +0530

    adding connections

---
+++ src/Data/connectionData.js
@@ -0,0 +1,18 @@
+const connectionData = [
+  // {
+  //   name: "Amrita Naik",
+  //   image:"https://img.freepik.com/premium-psd/3d-female-cartoon-character-avatar-isolated-3d-rendering_235528-956.jpg?w=740",
+  //   desc: "SIT-2017"  
+  //   },
+  //   {
+  //     name: "Abhinav Kar",
+  //     image:"https://img.freepik.com/premium-psd/character-avatar-3d-illustration_460336-710.jpg?w=740",
+  //     desc: "SIT-2017"
+  //     },
+  // {
+  //   name: "Dummy",
+  //   image:"https://img.freepik.com/premium-psd/3d-female-cartoon-character-avatar-isolated-3d-rendering_235528-956.jpg?w=740",
+  //   desc: "SIT-2017" 
+  //   }
+  ];
+  export default connectionData;
